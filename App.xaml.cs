516e9ca8039c16c80fd73253ecd9aa458f8963f0
Student <Student@LabVM2049939>
2026-05-10 22:14:27 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CyberSecurityAwarenessBotGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
