40736b29573d480f8d746c0028bfeb8d72db1635
Innocent Comfort <st10128118@rcconnect.edu.za>
2026-05-29 22:14:46 +0200

    Add SplashScreen and Start_Up windows with resources
    Created new `SplashScreen` and `Start_Up` windows to enhance the user interface. Added `CSB_logo.png` and `cybersec bot logo.png.png` as image resources. Updated `CyberSecurityAwarenessBotGUI.csproj` to include greeting audio files and necessary resource configurations.
    
    Refactored `App.xaml` to streamline namespace declarations. Implemented logic in `Start_Up.xaml.cs` for grid navigation and username validation. Added `System.Windows.Extensions` package reference for WPF enhancements.

---
+++ SplashScreen.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CyberSecurityAwarenessBot;
+using System.Windows;
+
+namespace CyberSecurityAwarenessBotGUI
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            WindowStyle = WindowStyle.None;
+        }
+    }
+}
---
+++ Start_Up.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CyberSecurityAwarenessBot;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace CyberSecurityAwarenessBotGUI
+{
+    /// <summary>
+    /// Interaction logic for Start_Up.xaml
+    /// </summary>
+    public partial class Start_Up : Window
+    {
+        public Start_Up()
+        {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+
+            // Play WAV greeting audio for greeting
+            Actions.PlayVoiceOrSound("greeting_audio.wav");
+        }
+
+        private void proceed(object sender, RoutedEventArgs e)
+        {
+            home_grid.Visibility = Visibility.Hidden;
+            username_grid.Visibility = Visibility.Visible;
+
+            // Play WAV greeting audio for greeting
+            Actions.PlayVoiceOrSound("greeting.wav");
+        }
+
+        private void submit_name(object sender, RoutedEventArgs e)
+        {
+            string username = usernames_input.Text.Trim();
+            if (string.IsNullOrWhiteSpace(username) || !Regex.IsMatch(username, @"^[a-zA-Z\s'-]{2,50}$"))
+            {
+                MessageBox.Show("Please enter a valid username.");
+                return;
+            }
+            MainWindow mainWindow = new MainWindow(username);
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
